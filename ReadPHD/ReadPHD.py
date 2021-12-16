7cb90de947925deeaeb616e4b3db4bcad102e81d
Timofeev Dobrynya <dobtimofeev@gmail.com>
2021-12-16 14:36:58 +0300

    in ReadPHD added strip method in init

--- ReadPHD/ReadPHD.py
+++ ReadPHD/ReadPHD.py
@@ -11,7 +11,7 @@ def is_float(value):
 
 
 def plot_histogram(matrix):
-    plt.pcolormesh(matrix, cmap='jet')
+    plt.imshow(matrix, cmap='jet', origin='lower')
     plt.colorbar()
     plt.show()
 
@@ -23,13 +23,14 @@ class ReadPHD:
         self.__path = path
         __file = open(path)
         self.__file_content = __file.readlines()
+        self.__file_content = [x.strip() for x in self.__file_content]
         __file.close()
 
     def __end_block_search(self, start):
         while True:
             start += 1
             s = self.__file_content[start]
-            if s.find('#') != -1 or s.rstrip() == '' or s == 'STOP\n':
+            if s.find('#') != -1 or s == 'STOP': # or s.rstrip() == '' removed becouse strip added in __init__
                 return start
 
     def __mat_to_list(self, ind):
@@ -130,47 +131,57 @@ class ReadPHD:
         print(f'In file {self.__path} #Header not found')
 
     def get_collection(self):
-        if self.__file_content.count('#Collection\n'):
-            ind = self.__file_content.index('#Collection\n')
+        if self.__file_content.count('#Collection'):
+            ind = self.__file_content.index('#Collection')
             s = self.__file_content[ind + 1].split()
             return s
         else:
             print(f'In file {self.__path} #Collection not found')
 
     def get_acquisition(self):
-        if self.__file_content.count('#Acquisition\n'):
-            ind = self.__file_content.index('#Acquisition\n')
+        if self.__file_content.count('#Acquisition'
+                                     ''):
+            ind = self.__file_content.index('#Acquisition'
+                                            '')
             s = self.__file_content[ind+1].split()
             return s
         else:
             print(f'In file {self.__path} #Acquisition not found')
 
     def get_calibration(self):
-        if self.__file_content.count('#Calibration\n'):
-            ind = self.__file_content.index('#Calibration\n')
+        if self.__file_content.count('#Calibration'
+                                     ''):
+            ind = self.__file_content.index('#Calibration'
+                                            '')
             s = self.__file_content[ind + 1].split()
             return s
         else:
             print(f'In file {self.__path} #Calibration not found')
 
     def get_processing(self):
-        if self.__file_content.count('#Processing\n'):
-            start = self.__file_content.index('#Processing\n') + 1
+        if self.__file_content.count('#Processing'
+                                     ''):
+            start = self.__file_content.index('#Processing'
+                                              '') + 1
             return [list(map(float, self.__file_content[i].split())) for i in range(start, start+3)]
         else:
             print(f'In file {self.__path} #Processing not found')
 
     def get_energy_g(self):
-        if self.__file_content.count('#g_Energy\n'):
-            start = self.__file_content.index('#g_Energy\n')+1
+        if self.__file_content.count('#g_Energy'
+                                     ''):
+            start = self.__file_content.index('#g_Energy'
+                                              '')+1
             return [list(map(float, self.__file_content[i].split()))
                     for i in range(start, self.__end_block_search(start))]
         else:
             print(f'In file {self.__path} #g_Energy not found')
 
     def get_energy_b(self):
-        if self.__file_content.count('#b_Energy\n'):
-            start = self.__file_content.index('#b_Energy\n') + 1
+        if self.__file_content.count('#b_Energy'
+                                     ''):
+            start = self.__file_content.index('#b_Energy'
+                                              '') + 1
             matrix = [self.__file_content[i].split() for i in range(start, self.__end_block_search(start))]
             [j.pop(1) for j in matrix]
             return [list(map(float, i)) for i in matrix]
@@ -178,71 +189,89 @@ class ReadPHD:
             print(f'In file {self.__path} #b_Energy not found')
 
     def get_resolution_g(self):
-        if self.__file_content.count('#g_Resolution\n'):
-            start = self.__file_content.index('#g_Resolution\n') + 1
+        if self.__file_content.count('#g_Resolution'
+                                     ''):
+            start = self.__file_content.index('#g_Resolution'
+                                              '') + 1
             return [list(map(float, self.__file_content[i].split()))
                     for i in range(start, self.__end_block_search(start))]
         else:
             print(f'In file {self.__path} #g_Resolution not found')
 
     def get_resolution_b(self):
-        if self.__file_content.count('#b_Resolution\n'):
-            start = self.__file_content.index('#b_Resolution\n') + 1
+        if self.__file_content.count('#b_Resolution'
+                                     ''):
+            start = self.__file_content.index('#b_Resolution'
+                                              '') + 1
             return [list(map(float, self.__file_content[i].split()))
                     for i in range(start, self.__end_block_search(start))]
         else:
             print(f'In file {self.__path} #b_Resolution not found')
 
     def get_limits_roi(self):
-        if self.__file_content.count('#ROI_Limits\n'):
-            start = self.__file_content.index('#ROI_Limits\n') + 1
+        if self.__file_content.count('#ROI_Limits'
+                                     ''):
+            start = self.__file_content.index('#ROI_Limits'
+                                              '') + 1
             return [list(map(float, self.__file_content[i].split()))
                     for i in range(start, self.__end_block_search(start))]
         else:
             print(f'In file {self.__path} #ROI_Limits not found')
 
     def get_efficiency_g(self):
-        if self.__file_content.count('#g_Efficiency\n'):
-            start = self.__file_content.index('#g_Efficiency\n') + 1
+        if self.__file_content.count('#g_Efficiency'
+                                     ''):
+            start = self.__file_content.index('#g_Efficiency'
+                                              '') + 1
             return [list(map(float, self.__file_content[i].split()))
                     for i in range(start, self.__end_block_search(start))]
         else:
             print(f'In file {self.__path} #g_Efficiency not found')
 
     def get_efficiency_b_g(self):
-        if self.__file_content.count('#b-gEfficiency\n'):
-            start = self.__file_content.index('#b-gEfficiency\n') + 1
+        if self.__file_content.count('#b-gEfficiency'
+                                     ''):
+            start = self.__file_content.index('#b-gEfficiency'
+                                              '') + 1
             return [list(map(float, [j for j in self.__file_content[i].split() if is_float(j)]))
                     for i in range(start, self.__end_block_search(start))]
         else:
             print(f'In file {self.__path} #b-gEfficiency not found')
 
     def get_ratios(self):
-        if self.__file_content.count('#Ratios\n'):
-            start = self.__file_content.index('#Ratios\n') + 1
-            return [list(map(float, [j for j in self.__file_content[i].split() if is_float(j)]))
+        if self.__file_content.count('#Ratios'
+                                     ''):
+            start = self.__file_content.index('#Ratios'
+                                              '') + 1
+            return [list(map(float, [j for j in self.__file_content[i].split() if isinstance(float(j), float)]))
                     for i in range(start, self.__end_block_search(start))]
         else:
             print(f'In file {self.__path} #Ratios not found')
 
     def get_spectrum_g(self):
-        if self.__file_content.count('#g_Spectrum\n'):
-            ind = self.__file_content.index('#g_Spectrum\n')
+        if self.__file_content.count('#g_Spectrum'
+                                     ''):
+            ind = self.__file_content.index('#g_Spectrum'
+                                            '')
             # return self.__mat_to_list(ind)
             return self.__mat_to_list(ind)
         else:
             print(f'In file {self.__path} #g_Spectrum not found')
 
     def get_spectrum_b(self):
-        if self.__file_content.count('#b_Spectrum\n'):
-            ind = self.__file_content.index('#b_Spectrum\n')
+        if self.__file_content.count('#b_Spectrum'
+                                     ''):
+            ind = self.__file_content.index('#b_Spectrum'
+                                            '')
             return self.__mat_to_list(ind)
         else:
             print(f'In file {self.__path} #b_Spectrum not found')
 
     def get_histogram(self):
-        if self.__file_content.count('#Histogram\n'):
-            ind = self.__file_content.index('#Histogram\n')
+        if self.__file_content.count('#Histogram'
+                                     ''):
+            ind = self.__file_content.index('#Histogram'
+                                            '')
             parse = self.__file_content[ind+1].split()
             matrix = [list(map(int, self.__file_content[ind+i+2].split())) for i in range(int(parse[0]))
                       if self.__file_content[ind+i+2]]
@@ -260,12 +289,13 @@ def safe_data(name, *args):
 
 
 def process_files():
-    #E:/Work/MIX_Files/Phase A/Red/SAMPLE
+    # E:/Work/MIX_Files/Phase A/Red/SAMPLE
     # for file in os.listdir("E:/Work/MIX_Files/2020 01-04/Blue"):
     #     temp = ReadPHD("E:/Work/MIX_Files/2020 01-04/Blue/" + file)
     #     collection = temp.get_collection()
     #     if temp.get_spectrum_qualifier() == 'FULL':
-    #         safe_data('Blue1', collection[0], '\t', collection[4], '\n')
+    #         safe_data('Blue1', collection[0], '\t', collection[4], '\n''
+    #         ')
     path = "E:/Work/MIX_Files/Phase A/Red/SAMPLE"
     for file in os.listdir(path):
         temp = ReadPHD(path + '/' + file)
@@ -279,19 +309,18 @@ def process_files():
 
 
 def main():
-    # spike = ReadPHD("E:/GitReps/PHDReader/MIX/SAMPLE/SAMPLE2020090419185213.PHD")
+    spike = ReadPHD("SPIKE20190502222812845.PHD")
+    # print(spike.get_ratios())
     # collection = spike.get_collection()
     # print(collection[0] > '2020/08/04')
-    # a = spike.get_histogram()
-    # plot_histogram(a)
-    process_files()
-
+    a = spike.get_histogram()
+    plot_histogram(a)
+    # process_files()
 
     # a = spike.get_data_type()
     # print(a)
     # str = spike.getTransmitDateTime().rstrip()
     # date_obj = datetime.datetime.strptime(str, '%Y/%m/%d %H:%M:%S.%f')
-
 
 if __name__ == '__main__':
     main()
